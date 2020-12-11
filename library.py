f6fbf90299d9453624a427d4ddb3e76ccaba2d66
miladoskuei <75860859+miladoskuei@users.noreply.github.com>
2020-12-12 00:31:33 +0330

    Add files via upload

--- library.py
+++ library.py
@@ -1,22 +1,22 @@
 n = int(input())
 list = []
+list1 = []
 import math
 for i in range(0,n):
     b = int(input())
     c = math.sqrt(b)
     c = str(c)
+    list1 = c.split('.')
+    adade_sahih = len(list1[0])
+    tedad = adade_sahih + 5
     if len(c) > 5:
-        d = c[:6]
+        d = c[:tedad]
         list.append(d)
-    elif len(c) == 3:
+    elif len(c) == 3 or len(c) == 4 or len(c) == 5:
         d = c + '000'
         list.append(d)
-        
-    
-print(list)
-
-
-
+for i in list:
+    print(i)
 
 
 
